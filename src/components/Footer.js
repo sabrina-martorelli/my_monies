d84e0a189eaa061d8b5b70d4e067e121e99d299e
SLM <sabrina.martorelli@gmail.com>
2023-04-07 19:46:53 +0100

    Amends footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,36 @@
-function Footer() {
-  return (
-    <>
-      <div className="footer">
- 
-        <h2 className="Copyright"> © 2023 </h2>
-      </div>
-    </>
-  );
-}
+import React from 'react';
 
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+
+//Component that renders a footer that includes 3  icons with Sabrina's info.
+const Footer = () => {
+    const [value, setValue] = React.useState(0);
+
+    return (
+      <Box  xs={12}
+     
+      >
+        
+        <BottomNavigation
+         style={{backgroundColor: "#e9c46a", fontSize: "22px",  color: '#fff',}}
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+            
+          <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/in/sabrina-martorelli" target="blank" />
+          <BottomNavigationAction label="Github" icon={<GitHubIcon />}  href="https://github.com/sabrina-martorelli/" target="blank" />
+          <BottomNavigationAction label="Email" icon={<EmailIcon />}  href="mailto:sabrina.martorelli@gmail.com" target="blank" />
+        </BottomNavigation>
+      </Box>
+    );
+}
+ 
 export default Footer;
